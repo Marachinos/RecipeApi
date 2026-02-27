dbc401addfdcfdc241c5ffb735c8c63b1947942b
Aigen80 <aomurm@gmail.com>
2026-02-27 11:31:05 +0100

    Lägg till DTO för att skapa ingrediens

--- RecipeApi/Models/DTOs/CreateIngredientDto.cs
+++ RecipeApi/Models/DTOs/CreateIngredientDto.cs
@@ -1,6 +1,16 @@
-﻿namespace RecipeApi.Models.DTOs
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace RecipeApi.Models.DTOs
 {
     public class CreateIngredientDto
     {
+        [Required]
+        public string Name { get; set; } = string.Empty;
+
+        [Range(0.001, double.MaxValue)]
+        public decimal Quantity { get; set; }
+
+        [Required]
+        public string Unit { get; set; } = string.Empty;
     }
 }
