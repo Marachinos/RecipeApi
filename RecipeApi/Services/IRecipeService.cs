7bf3b8df5581e0f6f03dce28f89f909614a988e0
MarikaRomeo <marika.romeo.syne25lin@edu.tucsweden.se>
2026-02-27 10:32:51 +0100

    Add IRecipeService interface with async CRUD methods
    Introduced the IRecipeService interface in the RecipeApi.Services namespace. This interface defines asynchronous methods for retrieving all recipes, fetching a recipe by ID, and creating a new recipe using a CreateRecipeDto.

---
+++ RecipeApi/Services/IRecipeService.cs
@@ -0,0 +1,10 @@
+﻿namespace RecipeApi.Services
+    {
+        public interface IRecipeService
+        {
+            Task<List<Recipe>> GetAllAsync();
+            Task<Recipe?> GetByIdAsync(int id);
+            Task<Recipe> CreateAsync(CreateRecipeDto dto);
+        }
+
+    }
