9698b00673152e8aacd046938201db9435f4561f
MarikaRomeo <marika.romeo.syne25lin@edu.tucsweden.se>
2026-02-27 10:33:18 +0100

    Add RecipeService with CRUD methods and DTO mapping
    Implemented RecipeService in RecipeApi.Services, providing methods to get all recipes, get by ID, and create new recipes from CreateRecipeDto. Service uses IRecipeRepository for data access and maps DTOs to Recipe entities with default values.

---
+++ RecipeApi/Services/RecipeService.cs
@@ -0,0 +1,47 @@
+﻿namespace RecipeApi.Services
+{
+        public class RecipeService : IRecipeService
+        {
+            private readonly IRecipeRepository _repository;
+
+            public RecipeService(IRecipeRepository repository)
+            {
+                _repository = repository;
+            }
+
+            public async Task<List<Recipe>> GetAllAsync()
+            {
+                return await _repository.GetAllAsync();
+            }
+
+            public async Task<Recipe?> GetByIdAsync(int id)
+            {
+                return await _repository.GetByIdAsync(id);
+            }
+
+            public async Task<Recipe> CreateAsync(CreateRecipeDto dto)
+            {
+                var recipe = new Recipe
+                {
+                    Name = dto.Name,
+                    Description = dto.Description,
+                    PrepTimeMinutes = dto.PrepTimeMinutes,
+                    CookTimeMinutes = dto.CookTimeMinutes,
+                    Servings = dto.Servings,
+                    Difficulty = "Easy",
+                    CreatedAt = DateTime.UtcNow,
+                    Ingredients = dto.Ingredients.Select(i => new Ingredient
+                    {
+                        Name = i.Name,
+                        Quantity = i.Quantity,
+                        Unit = i.Unit
+                    }).ToList(),
+                    Instructions = dto.Instructions
+                };
+
+                return await _repository.CreateAsync(recipe);
+            }
+        }
+
+    }
+}
